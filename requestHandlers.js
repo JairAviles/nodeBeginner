03a21bd9c6683b7d8c068630d95caa80340b78d6
Jair Avilés <hi@jairaviles.mx>
2015-08-25 13:26:56 -0500

    [Fix] Handling image uploaded withint the callback in fs.rename method

--- requestHandlers.js
+++ requestHandlers.js
@@ -5,18 +5,6 @@ var querystring = require('querystring'),
 
 function start(response) {
 	console.log("Request handler 'start' was called.");
-	// var body =  '<html>'+
-	// 			'<head>'+
-	// 			'<meta http-equiv="Content-Type" content="text/html; '+
-	// 			'charset=UTF-8" />'+
-	// 			'</head>'+
-	// 			'<body>'+
-	// 			'<form action="/upload" method="post">'+
-	// 			'<textarea name="text" rows="20" cols="60"></textarea>'+
-	// 			'<input type="submit" value="Submit text" />'+
-	// 			'</form>'+
-	// 			'</body>'+
-	// 			'</html>';
 var body =  '<html>'+
 			'<head>'+
 			'<meta http-equiv="Content-Type" '+
@@ -31,24 +19,6 @@ var body =  '<html>'+
 			'</body>'+
 			'</html>';
 
-	// function sleep(milliSeconds) {
-	// var startTime = new Date().getTime();
-	// while (new Date().getTime() < startTime + milliSeconds);
-	// }
-	// sleep(10000);
-	// return "Hello Start";
-	// var content = "empty";
-	// exec("ls -lah", function (error, stdout, stderr) { 
-	// 	content = stdout;
-	// });
-	// return content;
-	// exec("find /",
-	// 	{ timeout: 10000, maxbuffer: 20000*1024 }, 
-	// 	function(error, stdout, stderr){
-	// 	response.writeHead(200, {"Content-Type": "text/plain"});
-	// 	response.write(stdout);
-	// 	response.end();
-	// });
 	response.writeHead(200, {"Content-Type": "text/html"});
 	response.write(body);
 	response.end();
@@ -64,9 +34,7 @@ function upload(response, request) {
 		/* Possible error on Windows systems:
 		tried to rename to an already existing file */
 		console.log(files);
-		console.log(files.upload.path);
-		//fs.rename(files.upload.path, "/tmp/test.png", function(error) {
-		fs.rename(files.upload.path, "/tmp/test.png", function(error) {	
+		fs.rename(files.upload.path, "/tmp/test.png", function(error) {
 			if (error) {
 				fs.unlink("/tmp/test.png");
 				fs.rename(files.upload.path, "/tmp/test.png");
@@ -77,11 +45,6 @@ function upload(response, request) {
 		response.write("<img src='/show' />");
 		response.end();
 	});
-	// response.writeHead(200, {"Content-Type": "text/plain"});
-	//response.write("Hello Upload");
-	// response.write("You've sent the text: " +
-	// querystring.parse(postData).text);
-	// response.end();
 }
 
 function show(response) {
--- server.js
+++ server.js
@@ -16,33 +16,12 @@ var port = 8888;
 
  function start(route, handle) {
 	function onRequest(request, response) { 
-		// console.log("Request received."); 
-		// console.log('request.url: ' + request.url);
-		// var postData = "";
 		var pathname = url.parse(request.url).pathname;
 
 		//Discard request for handling '/favicon.ico' item
 		if(pathname.indexOf('/favicon.ico') === -1) {
 			console.log('Request for ' + pathname + ' received.');
 			route(handle, pathname, response, request);
-			// var content = route(handle, pathname);
-			// response.writeHead(200, {"Content-Type": "text/plain"});
-			// response.write("[HTTP] === Hello World ===");
-			// response.write(content);
-	  		// response.end();
-	  		// request.setEncoding('utf8');
-	  		//listeners call backs that handle the bulk of data by chunks
-	  		// The following event occur when there is data to handle
-	  		// request.addListener('data', function(postDataChunk){
-	  		// 	postData += postDataChunk;
-	  		// 	console.log("Recieved POST data chunk '" +
-	  		// 		 postDataChunk +
-	  		// 		 "' .");
-	  		// });
-	  		//The following event occur when the whole data has been handled
-	  		// request.addListener("end", function(){
-	  		// 	route(handle, pathname, response, postData);
-	  		// });
 		}
 	}
 	http.createServer(onRequest).listen(8888); 
